75395c2b3c64fcdd5eefc05e248cebffae6230bd
as13579e <1104063336@qq.com>
2017-08-03 20:45:26 +0800

    del

--- src/js/routes.js
+++ src/js/routes.js
@@ -8,39 +8,15 @@
 		}).state('index.topics',{
 			url: "/topics",
 			templateUrl:'./template/topics.html'
-		}).state('index.ask',{
-			url: "/ask",
-			templateUrl:'./template/ask.html'
-		}).state('index.share',{
-			url: "/share",
-			templateUrl:'./template/share.html'
-		}).state('index.job',{
-			url: "/job",
-			templateUrl:'./template/job.html'
-		}).state('index.good',{
-			url: "/good",
-			templateUrl:'./template/good.html'
 		}).state('detail',{
 			url: "/detail/:id",
 			templateUrl:'./template/detail.html'
-		}).state('about',{
-			url: "/about",
-			templateUrl:'./template/about.html'
 		}).state('login',{
 			url: "/login",
 			templateUrl:'./template/login.html'
 		}).state('register',{
 			url: "/register",
 			templateUrl:'./template/register.html'
-		}).state('getstart',{
-			url: "/getstart",
-			templateUrl:'./template/getstart.html'
-		}).state('mine',{
-			url: "/mine",
-			templateUrl:'./template/mine.html'
-		}).state('user',{
-			url: "/user/:username",
-			templateUrl:'./template/user.html'
 		})
 		$urlRouterProvider.when('','/index/topics')
 		
